f804f13ae3bc891e4ccbc46f664ee8920f4864d9
kevin <kevin@mythica.ai>
2024-06-24 11:12:45 -0700

    Inject menu option to open mythica panel

--- Source/Mythica/Private/Mythica.cpp
+++ Source/Mythica/Private/Mythica.cpp
@@ -2,17 +2,43 @@
 
 #include "Mythica.h"
 
+#include "Framework/MultiBox/MultiBoxBuilder.h"
+#include "LevelEditor.h"
+
 #define LOCTEXT_NAMESPACE "FMythicaModule"
 
 void FMythicaModule::StartupModule()
 {
-	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+	TSharedPtr<FExtender> MenuExtender = MakeShareable(new FExtender);
+	MenuExtender->AddMenuExtension(
+		"GetContent",
+		EExtensionHook::After,
+		nullptr,
+		FMenuExtensionDelegate::CreateRaw(this, &FMythicaModule::AddMenu));
+
+	FLevelEditorModule& LevelEditorModule = FModuleManager::LoadModuleChecked<FLevelEditorModule>("LevelEditor");
+	LevelEditorModule.GetMenuExtensibilityManager()->AddExtender(MenuExtender);
 }
 
 void FMythicaModule::ShutdownModule()
 {
 	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
 	// we call this function before unloading the module.
+}
+
+void FMythicaModule::AddMenu(FMenuBuilder& MenuBuilder)
+{
+	MenuBuilder.AddMenuEntry(
+		FText::FromString("Mythica Package Manager"),
+		FText::FromString("Opens the Mythica Package Manager"),
+		FSlateIcon(),
+		FUIAction(FExecuteAction::CreateRaw(this, &FMythicaModule::OnMenuClick))
+	);
+}
+
+void FMythicaModule::OnMenuClick()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Mythica menu pressed"));
 }
 
 #undef LOCTEXT_NAMESPACE
