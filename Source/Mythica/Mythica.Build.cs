d0965b1d418817487ab43ecf69a12a4f689292ad
kevin <kevin@mythica.ai>
2024-06-24 16:06:41 -0700

    Create an embedded UMG widget inside the slate window

--- Source/Mythica/Mythica.Build.cs
+++ Source/Mythica/Mythica.Build.cs
@@ -26,6 +26,7 @@ public class Mythica : ModuleRules
 			new string[]
 			{
 				"Core",
+                "UMG"
 				// ... add other public dependencies that you statically link with here ...
 			}
 			);
@@ -39,6 +40,7 @@ public class Mythica : ModuleRules
 				"Engine",
 				"Slate",
 				"SlateCore",
+                "UnrealEd"
 				// ... add private dependencies that you statically link with here ...	
 			}
 			);
