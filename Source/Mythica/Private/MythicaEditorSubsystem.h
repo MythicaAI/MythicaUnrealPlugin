b0370f7aacce1a7af12d40bd0bbf3bedd86605a3
kevin <kevin@mythica.ai>
2024-06-25 11:31:22 -0700

    Add example HTTP query

--- Source/Mythica/Private/MythicaEditorSubsystem.h
+++ Source/Mythica/Private/MythicaEditorSubsystem.h
@@ -2,7 +2,11 @@
 
 #include "CoreMinimal.h"
 #include "EditorSubsystem.h"
+#include "Interfaces/IHttpRequest.h"
+#include "Interfaces/IHttpResponse.h"
 #include "MythicaEditorSubsystem.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnFactReceived, const FString&, Fact);
 
 UCLASS()
 class UMythicaEditorSubsystem : public UEditorSubsystem
@@ -14,4 +18,12 @@ class UMythicaEditorSubsystem : public UEditorSubsystem
 
 public:
 
+	UFUNCTION(BlueprintCallable, Category = "Mythica")
+	void RequestFact();
+
+	UPROPERTY(BlueprintAssignable, Category = "Mythica")
+	FOnFactReceived OnFactReceived;
+
+private:
+	void OnResponseReceived(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
 };
