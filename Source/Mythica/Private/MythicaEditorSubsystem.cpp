145620b3c2125d45bc75a8a3ee6d84585c3cf662
kevin <kevin@mythica.ai>
2024-06-25 15:42:37 -0700

    Add session create flow

--- Source/Mythica/Private/MythicaEditorSubsystem.cpp
+++ Source/Mythica/Private/MythicaEditorSubsystem.cpp
@@ -1,5 +1,6 @@
 #include "MythicaEditorSubsystem.h"
 
+#include "MythicaDeveloperSettings.h"
 #include "HttpModule.h"
 
 void UMythicaEditorSubsystem::Initialize(FSubsystemCollectionBase& Collection)
@@ -12,22 +13,50 @@ void UMythicaEditorSubsystem::Deinitialize()
     Super::Deinitialize();
 }
 
-void UMythicaEditorSubsystem::RequestFact()
+void UMythicaEditorSubsystem::CreateSession()
 {
+    const UMythicaDeveloperSettings* Settings = GetDefault<UMythicaDeveloperSettings>();
+
+    if (Settings->ProfileId.IsEmpty())
+    {
+        UE_LOG(LogTemp, Error, TEXT("Profile ID is empty"));
+        return;
+    }
+
+    FString Url = FString::Printf(TEXT("http://localhost:50555/api/v1/profiles/start_session/%s"), *Settings->ProfileId);
+
     TSharedRef<IHttpRequest, ESPMode::ThreadSafe> Request = FHttpModule::Get().CreateRequest();
-    Request->SetURL("https://catfact.ninja/fact");
+    Request->SetURL(Url);
     Request->SetVerb("GET");
     Request->SetHeader("Content-Type", "application/json");
-    Request->OnProcessRequestComplete().BindUObject(this, &UMythicaEditorSubsystem::OnResponseReceived);
+    Request->OnProcessRequestComplete().BindUObject(this, &UMythicaEditorSubsystem::OnCreateSessionResponse);
 
     Request->ProcessRequest();
 }
 
-void UMythicaEditorSubsystem::OnResponseReceived(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful)
+void UMythicaEditorSubsystem::OnCreateSessionResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful)
 {
     if (!bWasSuccessful || !Response.IsValid())
+    {
+        UE_LOG(LogTemp, Error, TEXT("Failed to create session"));
         return;
+    }
 
     FString ResponseContent = Response->GetContentAsString();
-    OnFactReceived.Broadcast(ResponseContent);
+    TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(ResponseContent);
+
+    TSharedPtr<FJsonObject> JsonObject;
+    if (!FJsonSerializer::Deserialize(Reader, JsonObject) || !JsonObject.IsValid())
+    {
+        UE_LOG(LogTemp, Error, TEXT("Failed to parse JSON string"));
+        return;
+    }
+
+    FString Token;
+    if (!JsonObject->TryGetStringField(TEXT("token"), Token))
+    {
+        UE_LOG(LogTemp, Error, TEXT("Failed to get token from JSON string"));
+    }
+
+    AuthToken = Token;
 }
