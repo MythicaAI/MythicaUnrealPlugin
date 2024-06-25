12bbc433fcc5574caf3fcf990741950621d215df
kevin <kevin@mythica.ai>
2024-06-25 10:39:35 -0700

    Added editor subsystem for managing plugin state

---
+++ Source/Mythica/Private/MythicaEditorSubsystem.cpp
@@ -0,0 +1,11 @@
+#include "MythicaEditorSubsystem.h"
+
+void UMythicaEditorSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+    Super::Initialize(Collection);
+}
+
+void UMythicaEditorSubsystem::Deinitialize()
+{
+    Super::Deinitialize();
+}
