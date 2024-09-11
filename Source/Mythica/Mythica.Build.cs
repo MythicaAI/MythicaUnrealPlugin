d1b2b58ee9e8db882a30b1561c99ebd9b39cc3ae
kevin <kevin@mythica.ai>
2024-09-11 10:25:28 -0700

    Cleanup

--- Source/Mythica/Mythica.Build.cs
+++ Source/Mythica/Mythica.Build.cs
@@ -48,7 +48,7 @@ public class Mythica : ModuleRules
 				"InputCore",
 				"Json",
 				"MaterialBaking",
-                "Projects",
+				"Projects",
 				"Slate",
 				"SlateCore",
 				"UMGEditor",
