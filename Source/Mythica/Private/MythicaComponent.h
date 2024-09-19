bddfc96e880b858766cec31631dc4c37f27183ff
kevin <kevin@mythica.ai>
2024-09-19 15:00:13 -0700

    Cleanup tab/space inconsistency

--- Source/Mythica/Private/MythicaComponent.h
+++ Source/Mythica/Private/MythicaComponent.h
@@ -9,43 +9,43 @@
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class UMythicaComponent : public USceneComponent
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
 public:
-	UMythicaComponent();
+    UMythicaComponent();
 
-	void EndPlay(const EEndPlayReason::Type EndPlayReason);
+    void EndPlay(const EEndPlayReason::Type EndPlayReason);
 
-	UFUNCTION(BlueprintCallable, CallInEditor, Category = "Mythica")
-	void RegenerateMesh();
+    UFUNCTION(BlueprintCallable, CallInEditor, Category = "Mythica")
+    void RegenerateMesh();
 
-	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+    virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
 
 private:
     void OnJobDefIdChanged();
 
-	UFUNCTION()
-	void OnJobStateChanged(int InRequestId, EMythicaJobState State);
+    UFUNCTION()
+    void OnJobStateChanged(int InRequestId, EMythicaJobState State);
 
-	void UpdateMesh();
+    void UpdateMesh();
 
 public:
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mythica")
-	FString JobDefId;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mythica")
+    FString JobDefId;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mythica")
-	FMythicaInputs Inputs;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mythica")
+    FMythicaInputs Inputs;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mythica")
-	FMythicaParameters Parameters;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mythica")
+    FMythicaParameters Parameters;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mythica")
-	FMythicaMaterialParameters MaterialParameters;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mythica")
+    FMythicaMaterialParameters MaterialParameters;
 
 private:
-	UPROPERTY()
-	int RequestId = -1;
+    UPROPERTY()
+    int RequestId = -1;
 
-	UPROPERTY()
-	TArray<FName> MeshComponentNames;
+    UPROPERTY()
+    TArray<FName> MeshComponentNames;
 };
