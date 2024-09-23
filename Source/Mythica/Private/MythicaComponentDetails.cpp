553bafd3a5fdee97c67239bdd25a0a2a1c3f926d
kevin <kevin@mythica.ai>
2024-09-23 13:47:31 -0700

    Add error feedback if failed to authenticate

--- Source/Mythica/Private/MythicaComponentDetails.cpp
+++ Source/Mythica/Private/MythicaComponentDetails.cpp
@@ -28,54 +28,88 @@ void FMythicaComponentDetails::CustomizeDetails(IDetailLayoutBuilder& DetailBuil
     MyCategory.AddCustomRow(FText::FromString("RegenerateMeshRow"))
         .WholeRowContent()
         [
-            SNew(SHorizontalBox)
-                + SHorizontalBox::Slot()
-                .HAlign(HAlign_Center)
-                .VAlign(VAlign_Center)
-                .FillWidth(0.5f)
-                [
-                    SNew(SButton)
-                        .Text(FText::FromString("Regenerate Mesh"))
-                        .IsEnabled_Lambda([ComponentWeak]()
-                        {
-                            if (ComponentWeak.IsValid())
-                            {
-                                EMythicaJobState State = ComponentWeak->GetJobState();
-                                return State == EMythicaJobState::Invalid ||
-                                       State == EMythicaJobState::Failed ||
-                                       State == EMythicaJobState::Completed;
-                            }
-
-                            return false;
-                        })
-                        .OnClicked_Lambda([ComponentWeak]()
-                        {
-                            if (ComponentWeak.IsValid())
-                            {
-                                ComponentWeak->RegenerateMesh();
-                            }
-                            return FReply::Handled();
-                        })
-                ]
-                + SHorizontalBox::Slot()
-                .HAlign(HAlign_Center)
-                .VAlign(VAlign_Center)
-                .FillWidth(0.5f)
+            SNew(SVerticalBox)
+                + SVerticalBox::Slot()
+                .AutoHeight()
+                .Padding(0, 10, 0, 10)
                 [
                     SNew(STextBlock)
+                        .Justification(ETextJustify::Center)
+                        .Visibility_Lambda([]()
+                        {
+                            UMythicaEditorSubsystem* MythicaEditorSubsystem = GEditor->GetEditorSubsystem<UMythicaEditorSubsystem>();
+                            if (MythicaEditorSubsystem->GetSessionState() != EMythicaSessionState::SessionCreated)
+                            {
+                                return EVisibility::Visible;
+                            }
+                            else
+                            {
+                                return EVisibility::Collapsed;
+                            }
+                        })
                         .Text_Lambda([ComponentWeak]()
                         {
-                            if (ComponentWeak.IsValid())
-                            {
-                                EMythicaJobState State = ComponentWeak->GetJobState();
-                                if (State != EMythicaJobState::Invalid && State != EMythicaJobState::Completed)
-                                {
-                                    FString StateString = StaticEnum<EMythicaJobState>()->GetNameStringByValue(static_cast<int64>(State));
-                                    return FText::FromString(StateString);
-                                }
-                            }
-                            return FText::FromString("");
+                            return FText::FromString("Failed to connect to Mythica service");
                         })
+                        .ColorAndOpacity(FLinearColor::Red)
+                ]
+                + SVerticalBox::Slot()
+                .AutoHeight()
+                [
+                    SNew(SHorizontalBox)
+                        + SHorizontalBox::Slot()
+                        .HAlign(HAlign_Center)
+                        .VAlign(VAlign_Center)
+                        .FillWidth(0.5f)
+                        [
+                            SNew(SButton)
+                                .Text(FText::FromString("Regenerate Mesh"))
+                                .IsEnabled_Lambda([ComponentWeak]()
+                                {
+                                    UMythicaEditorSubsystem* MythicaEditorSubsystem = GEditor->GetEditorSubsystem<UMythicaEditorSubsystem>();
+                                    if (MythicaEditorSubsystem->GetSessionState() != EMythicaSessionState::SessionCreated)
+                                    {
+                                        return false;
+                                    }
+
+                                    if (ComponentWeak.IsValid())
+                                    {
+                                        EMythicaJobState State = ComponentWeak->GetJobState();
+                                        return State == EMythicaJobState::Invalid ||
+                                               State == EMythicaJobState::Failed ||
+                                               State == EMythicaJobState::Completed;
+                                    }
+                                    return false;
+                                })
+                                .OnClicked_Lambda([ComponentWeak]()
+                                {
+                                    if (ComponentWeak.IsValid())
+                                    {
+                                        ComponentWeak->RegenerateMesh();
+                                    }
+                                    return FReply::Handled();
+                                })
+                        ]
+                        + SHorizontalBox::Slot()
+                        .HAlign(HAlign_Center)
+                        .VAlign(VAlign_Center)
+                        .FillWidth(0.5f)
+                        [
+                            SNew(STextBlock)
+                                .Text_Lambda([ComponentWeak]()
+                                {
+                                    if (ComponentWeak.IsValid())
+                                    {
+                                        EMythicaJobState State = ComponentWeak->GetJobState();
+                                        if (State != EMythicaJobState::Invalid && State != EMythicaJobState::Completed)
+                                        {
+                                            FString StateString = StaticEnum<EMythicaJobState>()->GetNameStringByValue(static_cast<int64>(State));
+                                            return FText::FromString(StateString);
+                                        }
+                                    }
+                                    return FText::FromString("");
+                                })
+                        ]
                 ]
         ];
 }
