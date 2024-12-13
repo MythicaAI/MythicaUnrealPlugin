128572140725f600ac15b5aa66fbf54308be4847
Kevin Calderone <kevincalderone@gmail.com>
2024-12-13 09:09:25 -0800

    Update default tool list (#145)
    Co-authored-by: kevin <kevin@mythica.ai>

--- Source/Mythica/Private/MythicaDeveloperSettings.cpp
+++ Source/Mythica/Private/MythicaDeveloperSettings.cpp
@@ -3,8 +3,10 @@
 const TCHAR* DefaultJobDefIdWhitelist[] =
 {
     TEXT("jobdef_2crsbDnsSuFXgZouExzetpMDyDNq"),    // Crystal
-    TEXT("jobdef_2ksoWHctAoAFcNN5roDzmSZ9iN62"),    // MeanderingVine
-    TEXT("jobdef_3o41eujjxzNCSiMWkEXteaeUEEEM"),    // Rockify
+    TEXT("jobdef_4Cf9gnbAjoTKdRsavFGAEXeDSLF9"),    // MeanderingVine
+    TEXT("jobdef_36esG96RhZBu4R7bMXAkKNnjKtrE"),    // Rockify
+    TEXT("jobdef_4V1RAwMKKbtbdqaZYVqAjCRWGdH8"),    // CrystalCluster
+    TEXT("jobdef_44tBgRfFD49UiSKhcKotnD2SGrqc"),    // MossGrowth
 };
 
 UMythicaDeveloperSettings::UMythicaDeveloperSettings(const FObjectInitializer& ObjectInitializer)
